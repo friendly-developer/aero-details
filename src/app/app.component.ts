a9468d46b675ea91afb069525004afa1c1317286
Gonuguntla <v747160@global.ual.com>
2018-02-05 17:14:55 +0530

    feat: Create Basic sigin form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { I18nSelectPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public lang: String = 'en';
+  public translatedCode: any = {
+    'en': 'Booking Code',
+    'other': 'Booking Code',
+  };
+  public translatedName: any = {
+    'en': 'Family Name',
+    'other': 'Family Name',
+
+  };
+  public translatedSubmit: any = {
+    'en': 'Retrieve booking',
+    'other': 'Retrieve booking',
+
+  };
+  constructor() {
+
+  }
+  ngOnInit() {
+
+  }
 }
